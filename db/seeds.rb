b98f9ac8dac50a48eabb4c8ec5b3b800b7624717
Dan <dansification@gmail.com>
2023-07-06 11:50:42 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Student.destroy_all
+
+Student.create!([
+  {
+  name: "Sole Barboza",
+  email: "sole@email.com"
+  },
+  {
+    name: "Dan Pariente",
+    email: "dan@email.com"
+  },
+])
+
+p "Created #{Student.count} students"
